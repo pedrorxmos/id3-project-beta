98c6aa48614069bb5dffc76a9ff743f94a68a2fd
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-15 22:23:07 +0100

    💄 feat: add hero banners as template parts

--- wp-content/themes/eyeconbeauty/front-page.php
+++ wp-content/themes/eyeconbeauty/front-page.php
@@ -1,25 +1,23 @@
 <?php get_header(); ?>
   <main>
-    <a href="#banner" class="herobanner">
-      <div class="herobanner__img" style="background-image: url(<?php echo get_the_post_thumbnail_url(104) ?>);"></div>
+    <?php 
+      get_template_part(
+        'template-parts/hero-banner', 
+        'hero-banner', 
+        array(
+          'id'	=> '104',
+        )
+      ) 
+    ?>
 
-      <div class="herobanner__info">
-        <h2><?php echo get_the_title(104); ?></h2>
-        <p><?php echo get_post_field('post_content', 104) ?></p>
-
-        <button class="btn btn-main">Shop Now</button>
-      </div>
-    </a>
-
-    <a href="#banner" class="herobanner">
-      <div class="herobanner__img" style="background-image: url(<?php echo get_the_post_thumbnail_url(108) ?>);"></div>
-
-      <div class="herobanner__info">
-        <h2><?php echo get_the_title(108); ?></h2>
-        <p><?php echo get_post_field('post_content', 108) ?></p>
-
-        <button class="btn btn-main">Shop Now</button>
-      </div>
-    </a>
+    <?php 
+      get_template_part(
+        'template-parts/hero-banner', 
+        'hero-banner', 
+        array(
+          'id'	=> '108',
+        )
+      ) 
+    ?>
   </main>
 <?php get_footer(); ?>
