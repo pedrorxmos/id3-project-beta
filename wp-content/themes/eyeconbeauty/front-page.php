02db802a13e321733c3e730301f09bc8ed83660d
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-15 20:39:05 +0100

    💄✨ feat: add herobanners to home and wp dashboard

--- wp-content/themes/eyeconbeauty/front-page.php
+++ wp-content/themes/eyeconbeauty/front-page.php
@@ -1,11 +1,22 @@
 <?php get_header(); ?>
   <main>
     <a href="#banner" class="herobanner">
-      <div class="herobanner__img"></div>
+      <div class="herobanner__img" style="background-image: url(<?php echo get_the_post_thumbnail_url(104) ?>);"></div>
 
       <div class="herobanner__info">
-        <h2>Title</h2>
-        <p>Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Quisque velit nisi, pretium ut lacinia in, elementum id enim.</p>
+        <h2><?php echo get_the_title(104); ?></h2>
+        <p><?php echo get_post_field('post_content', 104) ?></p>
+
+        <button class="btn btn-main">Shop Now</button>
+      </div>
+    </a>
+
+    <a href="#banner" class="herobanner">
+      <div class="herobanner__img" style="background-image: url(<?php echo get_the_post_thumbnail_url(108) ?>);"></div>
+
+      <div class="herobanner__info">
+        <h2><?php echo get_the_title(108); ?></h2>
+        <p><?php echo get_post_field('post_content', 108) ?></p>
 
         <button class="btn btn-main">Shop Now</button>
       </div>
--- wp-content/themes/eyeconbeauty/functions.php
+++ wp-content/themes/eyeconbeauty/functions.php
@@ -1,172 +1,169 @@
 <?php
 
-//  CARGAR JS Y CSS
+	//  CARGAR JS Y CSS
 
-    function my_scripts_and_css()
-    {
-        //  QUITAR GUTENBERG CSS
+	function my_scripts_and_css()
+	{
+		//  QUITAR GUTENBERG CSS
 
-        wp_dequeue_style ( 'wp-block-library' );
-        wp_dequeue_style ( 'wp-block-library-theme' );
-        wp_dequeue_style ( 'wc-blocks-style' );
+		wp_dequeue_style ( 'wp-block-library' );
+		wp_dequeue_style ( 'wp-block-library-theme' );
+		wp_dequeue_style ( 'wc-blocks-style' );
 
-        add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+		add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
 
-        //  CARGA MIS ESTILOS Y CSS 
-        
-        if ( !is_admin() ) { //Si no es en admin
+		//  CARGA MIS ESTILOS Y CSS 
+		
+		if ( !is_admin() ) { //Si no es en admin
 
-            // if( is_page('Contact')) { //Si es la página Contact
+			// if( is_page('Contact')) { //Si es la página Contact
 
-            // wp_enqueue_script ( 'js-topbar', get_stylesheet_directory_uri() . './js/topbar.js' );
-        
-            // wp_enqueue_style ( 'css-main', get_stylesheet_uri() );
-            wp_enqueue_script ( 'js-topbar', get_stylesheet_directory_uri() . '/js/topbar.js', array('jquery'), '1.0.0', true, true );
-            wp_enqueue_style ( 'css-main', get_stylesheet_uri(), '', filemtime ( get_template_directory() . '/style.css' ) );
-			
-        }
-    }
+			// wp_enqueue_script ( 'js-topbar', get_stylesheet_directory_uri() . './js/topbar.js' );
+	
+			// wp_enqueue_style ( 'css-main', get_stylesheet_uri() );
+			wp_enqueue_script ( 'js-topbar', get_stylesheet_directory_uri() . '/js/topbar.js', array('jquery'), '1.0.0', true, true );
+			wp_enqueue_style ( 'css-main', get_stylesheet_uri(), '', filemtime ( get_template_directory() . '/style.css' ) );
 
-    add_action ( 'wp_enqueue_scripts', 'my_scripts_and_css', 100 ); //ejecuta el script y los estilos
+		}
+	}
 
-
-
-//  ELIMINAR TAMAÑOS DE IMÁGENES QUE NO NECESITE MI SITIO WEB
-
-    remove_image_size ( '1536x1536' );
-    remove_image_size ( '2048x2048' );
-
-    update_option ( 'medium_large_size_w', '0' ); // Esta y la siguiente línea sirven para eliminar el tamaño 'medium_large'
-    update_option ( 'medium_large_size_h', '0' );
+	add_action ( 'wp_enqueue_scripts', 'my_scripts_and_css', 100 ); //ejecuta el script y los estilos
 
 
 
-//  AÑADIR SOPORTE PARA...
+	//  ELIMINAR TAMAÑOS DE IMÁGENES QUE NO NECESITE MI SITIO WEB
 
-    add_theme_support ( 'title-tag' );
-    add_theme_support ( 'post-thumbnails' );
+	remove_image_size ( '1536x1536' );
+	remove_image_size ( '2048x2048' );
 
-    // add_post_type_support ( 'page', 'excerpt' );
-
-
-
-//  REGISTRO DE MENÚS
-
-    register_nav_menu ( 'header-menu', 'Menu principal del sitio' );
-    register_nav_menu ( 'socialmedia-menu', 'Redes sociales del footer' );
-    register_nav_menu ( 'shop-menu', 'Menu de la tienda en el footer' );
-		register_nav_menu ( 'help-menu', 'Menu de ayuda en el footer' );
-		register_nav_menu ( 'company-menu', 'Menu de la compañia en el footer' );
-		register_nav_menu ( 'legal-menu', 'Menu legal de la tienda en el footer' );
+	update_option ( 'medium_large_size_w', '0' ); // Esta y la siguiente línea sirven para eliminar el tamaño 'medium_large'
+	update_option ( 'medium_large_size_h', '0' );
 
 
 
-//  REGISTRO DE SIDEBARS
+	//  AÑADIR SOPORTE PARA...
 
-    register_sidebar ( array
-    (
-        'id' => 'post-sidebar',
-        'name' => 'Sidebar en entradas',
-        'before_title' => '<h4>',
-        'after_title' => '</h4>',
-        'before_widget' => '<div class="widget %2$s">',
-        'after_widget' => '</div>',
-    ));
+	add_theme_support ( 'title-tag' );
+	add_theme_support ( 'post-thumbnails' );
 
-    register_sidebar ( array
-    (
-        'id' => 'page-sidebar',
-        'name' => 'Sidebar en páginas',
-        'before_title' => '<h4>',
-        'after_title' => '</h4>',
-        'before_widget' => '<div class="widget %2$s">',
-        'after_widget' => '</div>',
-    ));
-
-    //  REGISTRO DE TIPOS DE ENTRADA Y TAXONOMÍAS
-
-    // function my_custom_post_types_and_taxonomies()
-    // {
-    //     register_post_type ( 'fotofolio', array
-    //     (
-    //         'public' => true,
-    //         'exclude_from_search' => false,
-    //         'hierarchical' => false,
-    //         'menu_position' => 2,
-    //         'supports' => array
-    //         (
-    //             'title',
-    //             'editor',
-    //             'author',
-    //             'thumbnail',
-    //             'excerpt',
-    //             'revisions',
-    //             'custom-fields'
-    //         ),
-    //         'show_in_rest' => true, // Habilita Gutenberg
-    //         'labels' => array
-    //         (
-    //             'name' => __( 'Fotofolio', 'loosing' ), // Si queremos internacionalizar estas cadenas...
-    //             'singular_name' => 'Foto',
-    //             'add_new' => 'Añadir nueva foto',
-    //             'add_new_item' => 'Añadir nueva foto',
-    //             'all_items' => 'Todas los fotos',
-    //             'edit_item' => 'Editar foto',
-    //             'new_item' => 'Nueva foto',
-    //             'view_item' => 'Ver foto',
-    //             'search_items' => 'Buscar fotos',
-    //             'not_found' =>  'No se han encontrado fotos',
-    //             'not_found_in_trash' => 'No se han encontrado fotos en la papelera'
-    //         )
-    //     ));
-
-    //     register_taxonomy ( 'place', 'fotofolio', array
-    //     (
-    //         'show_admin_column' => true,
-    //         'hierarchical' => false,
-    //         'show_in_rest' => true,
-    //         'labels' => array
-    //         (
-    //             'name' => 'Lugares',
-    //             'singular_name' => 'Lugar',
-    //             'search_items' => 'Buscar lugares',
-    //             'all_items' => 'Todos los lugares',
-    //             'view_item' => 'Ver lugar',
-    //             'edit_item' => 'Editar lugar',
-    //             'update_item' => 'Actualizar lugar',
-    //             'add_new' => 'Añadir nuevo lugar',
-    //             'add_new_item' => 'Añadir nuevo lugar',
-    //             'new_item_name' => 'Nuevo nombre de lugar',
-    //             'not_found' => 'No se han encontrado lugares.',
-    //             'back_to_items' => __( 'Volver a Lugares', 'loosing' ),
-    //             'menu_name' => __( 'Lugares', 'loosing' )
-    //         )
-    //     ));
-    // }
-
-    // add_action ( 'init', 'my_custom_post_types_and_taxonomies' );
+	// add_post_type_support ( 'page', 'excerpt' );
 
 
-    //  ACTIVACIÓN DE LAS PÁGINAS DE OPCIONES DE ACF
-    
-    // acf_add_options_page ( array
-    // (
-    //     'page_title' => 'Configuración del sitio web',
-    //     'menu_slug' => 'config'
-    // ));
-    
-    // acf_add_options_sub_page ( array
-    // (
-    //     'page_title' => 'Personalización',
-    //     'parent_slug' => 'config',
-    // ));
-    
-    // acf_add_options_sub_page ( array
-    // (
-    //     'page_title' => 'Scripts',
-    //     'parent_slug' => 'config',
-    // ));
 
-    
+	//  REGISTRO DE MENÚS
+
+	register_nav_menu ( 'header-menu', 'Menu principal del sitio' );
+	register_nav_menu ( 'socialmedia-menu', 'Redes sociales del footer' );
+	register_nav_menu ( 'shop-menu', 'Menu de la tienda en el footer' );
+	register_nav_menu ( 'help-menu', 'Menu de ayuda en el footer' );
+	register_nav_menu ( 'company-menu', 'Menu de la compañia en el footer' );
+	register_nav_menu ( 'legal-menu', 'Menu legal de la tienda en el footer' );
+
+
+
+	//  REGISTRO DE SIDEBARS
+
+	// register_sidebar ( array
+	// (
+	// 	'id' => 'post-sidebar',
+	// 	'name' => 'Sidebar en entradas',
+	// 	'before_title' => '<h4>',
+	// 	'after_title' => '</h4>',
+	// 	'before_widget' => '<div class="widget %2$s">',
+	// 	'after_widget' => '</div>',
+	// ));
+
+	// register_sidebar ( array
+	// (
+	// 	'id' => 'page-sidebar',
+	// 	'name' => 'Sidebar en páginas',
+	// 	'before_title' => '<h4>',
+	// 	'after_title' => '</h4>',
+	// 	'before_widget' => '<div class="widget %2$s">',
+	// 	'after_widget' => '</div>',
+	// ));
+
+	//  REGISTRO DE TIPOS DE ENTRADA Y TAXONOMÍAS
+
+	function my_custom_post_types_and_taxonomies()
+	{
+		register_post_type ( 'hero-banner', array
+		(
+			'public' => true,
+			'exclude_from_search' => false,
+			'hierarchical' => false,
+			'supports' => array
+			(
+				'title',
+				'editor',
+				'author',
+				'thumbnail',
+				'excerpt',
+				'revisions',
+				'custom-fields'
+			),
+			'show_in_rest' => true, // Habilita Gutenberg
+			'labels' => array
+			(
+				'name' => __( 'Hero Banner', 'eyecon' ), // Si queremos internacionalizar estas cadenas...
+				'singular_name' => 'Banner',
+				'add_new' => 'Añadir nuevo banner',
+				'add_new_item' => 'Añadir nuevo banner',
+				'all_items' => 'Todas los banners',
+				'edit_item' => 'Editar banner',
+				'new_item' => 'Nueva banner',
+				'view_item' => 'Ver banner',
+				'search_items' => 'Buscar banner',
+				'not_found' =>  'No se han encontrado banners',
+				'not_found_in_trash' => 'No se han encontrado banners en la papelera'
+			)
+		));
+
+		// register_taxonomy ( 'place', 'fotofolio', array
+		// (
+		// 	'show_admin_column' => true,
+		// 	'hierarchical' => false,
+		// 	'show_in_rest' => true,
+		// 	'labels' => array
+		// 	(
+		// 		'name' => 'Lugares',
+		// 		'singular_name' => 'Lugar',
+		// 		'search_items' => 'Buscar lugares',
+		// 		'all_items' => 'Todos los lugares',
+		// 		'view_item' => 'Ver lugar',
+		// 		'edit_item' => 'Editar lugar',
+		// 		'update_item' => 'Actualizar lugar',
+		// 		'add_new' => 'Añadir nuevo lugar',
+		// 		'add_new_item' => 'Añadir nuevo lugar',
+		// 		'new_item_name' => 'Nuevo nombre de lugar',
+		// 		'not_found' => 'No se han encontrado lugares.',
+		// 		'back_to_items' => __( 'Volver a Lugares', 'loosing' ),
+		// 		'menu_name' => __( 'Lugares', 'loosing' )
+		// 		)
+		// ));
+	}
+
+	add_action ( 'init', 'my_custom_post_types_and_taxonomies' );
+
+
+	//  ACTIVACIÓN DE LAS PÁGINAS DE OPCIONES DE ACF
+	
+	// acf_add_options_page ( array
+	// (
+	// 	'page_title' => 'Configuración del sitio web',
+	// 	'menu_slug' => 'config'
+	// ));
+	
+	// acf_add_options_sub_page ( array
+	// (
+	// 	'page_title' => 'Personalización',
+	// 	'parent_slug' => 'config',
+	// ));
+	
+	// acf_add_options_sub_page ( array
+	// (
+	// 	'page_title' => 'Scripts',
+	// 	'parent_slug' => 'config',
+	// ));  
   
 ?>
