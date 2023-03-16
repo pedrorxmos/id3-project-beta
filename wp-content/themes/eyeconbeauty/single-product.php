50b204b5307997e9260c0b1a38f1d96e0267fb16
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-17 00:11:43 +0100

    💄 feat: specifications to pages

--- wp-content/themes/eyeconbeauty/single-product.php
+++ wp-content/themes/eyeconbeauty/single-product.php
@@ -1,6 +1,6 @@
 <?php get_header(); the_post();?>
   <main>
-    page.php
+    single-product.php
     <h1 class="hide"><?php the_title(); ?></h1>
     <div class="container gap-2rem">
       <h2><?php the_title(); ?></h2>
