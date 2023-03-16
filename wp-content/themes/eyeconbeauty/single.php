368654c1f54f1422ef2307c4ec9501c69cd0b5ee
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-16 23:38:14 +0100

    💄 feat: add single structure

--- wp-content/themes/eyeconbeauty/single.php
+++ wp-content/themes/eyeconbeauty/single.php
@@ -1,1 +1,9 @@
-single.php
+<?php get_header(); the_post();?>
+  <main>
+    <h1 class="hide"><?php the_title(); ?></h1>
+    <div class="container gap-2rem">
+      <h2><?php the_title(); ?></h2>
+      <?php the_content() ?>
+    </div>
+  </main>
+<?php get_footer(); ?>
