1d170be92fe9c3fc311cc9268c7a87ee5386653c
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-19 16:38:28 +0100

    💄 fix: result count text

--- wp-content/themes/eyeconbeauty/woocommerce/loop/result-count.php
+++ wp-content/themes/eyeconbeauty/woocommerce/loop/result-count.php
@@ -25,15 +25,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<?php
 	// phpcs:disable WordPress.Security
 	if ( 1 === intval( $total ) ) {
-		_e( 'Showing the single result', 'woocommerce' );
+		// _e( 'Showing the single result', 'woocommerce' );
+		_e( '1 Item', 'woocommerce' );
 	} elseif ( $total <= $per_page || -1 === $per_page ) {
 		/* translators: %d: total results */
-		printf( _n( 'Showing all %d result', 'Showing all %d results', $total, 'woocommerce' ), $total );
+		// printf( _n( 'Showing all %d result', 'Showing all %d results', $total, 'woocommerce' ), $total );
+		printf( _n( '%d Item', '%d Items', $total, 'woocommerce' ), $total );
 	} else {
 		$first = ( $per_page * $current ) - $per_page + 1;
 		$last  = min( $total, $per_page * $current );
 		/* translators: 1: first result 2: last result 3: total results */
-		printf( _nx( 'Showing %1$d&ndash;%2$d of %3$d result', 'Showing %1$d&ndash;%2$d of %3$d results', $total, 'with first and last result', 'woocommerce' ), $first, $last, $total );
+		// printf( _nx( 'Showing %1$d&ndash;%2$d of %3$d result', 'Showing %1$d&ndash;%2$d of %3$d results', $total, 'with first and last result', 'woocommerce' ), $first, $last, $total );
+		printf( _nx( '%1$d&ndash;%2$d of %3$d Item', '%1$d&ndash;%2$d of %3$d Items', $total, 'with first and last result', 'woocommerce' ), $first, $last, $total );
 	}
 	// phpcs:enable WordPress.Security
 	?>
