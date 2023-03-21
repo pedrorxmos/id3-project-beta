305b2239c35f6f2028cca7755350565fd716a49c
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-21 20:39:31 +0100

    💄 feat: front-page product sliders

--- wp-content/themes/eyeconbeauty/front-page.php
+++ wp-content/themes/eyeconbeauty/front-page.php
@@ -10,7 +10,74 @@
       ) 
     ?>
 
+  <div class="product-slider">
+    <div class="product-slider-title">
+      <h2>New releases</h2>
+      <h3>Check out our new products</h3>
+    </div>
+    <div class="product-slider__wrapper">
+      <?php
+        $params = array(
+          'posts_per_page' => 8,
+          'post_type' => array('product', 'product_variation'),
+          'post__not_in' => array(312, 148), //Exclude general shimmer and matte shadow
+          'orderby'        => 'date',
+          );
 
+        $wc_query = new WP_Query($params);
+      ?>
+
+      <?php if ($wc_query->have_posts()) : ?>
+      <?php while ($wc_query->have_posts()) : $wc_query->the_post(); ?>
+        <div class="product-card product-slider__card">
+        <a href="<?php the_permalink() ?>">
+            <?php the_post_thumbnail();?>
+          </a>
+          
+          <div class="product-info">
+            <a href="<?php the_permalink() ?>">
+              <div class="product-info__info">
+                <p class="product-info__category">
+                  <!-- Category  -->
+									<?php
+									$terms = get_the_terms( $product->ID, 'product_cat' );
+									foreach ($terms as $term) {
+											$product_cat_id = $term->term_id;
+											echo get_the_category_by_ID($product_cat_id);
+											break;
+									} 
+									?>
+                </p>
+                <p class="product-info__name">
+                  <?php the_title(); ?>
+                </p>
+                <div class="product-info__price">
+                  <?php echo wc_price($product->get_price()); ?>
+
+                  <?php if($product->is_on_sale()) : ?>
+                    <span class="price__discount">
+                      <?php echo wc_price($product->get_regular_price() ); ?>
+                    </span>
+                  <?php endif; ?>
+                </div>
+              </div>
+            </a>
+
+						<?php do_action( 'woocommerce_after_shop_loop_item' ); 
+						?>
+          </div>
+        </div>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?>
+      <?php else:  ?>
+      <p>
+        <?php _e( 'No Products'); ?>
+      </p>
+      <?php endif; ?>
+    </div>
+
+    <a href="<?php echo home_url( '/shop/' ); ?>" class="btn btn-main product-slider__btn">Shop Products</a>
+  </div>
     <?php 
       get_template_part(
         'template-parts/hero-banner', 
@@ -19,6 +86,76 @@
           'slug'	=> 'home-banner-2',
         )
       ) 
+    ?>
+<div class="product-slider">
+    <div class="product-slider-title">
+      <h2>Best Sellers</h2>
+      <h3>check what's trending right now!</h3>
+    </div>
+    <div class="product-slider__wrapper">
+      <?php
+        $params = array(
+          'posts_per_page' => 8,
+          'post_type' => array('product', 'product_variation'),
+          'post__not_in' => array(312, 148), //Exclude general shimmer and matte shadow
+          'orderby'        => 'popularity',
+          );
+
+        $wc_query = new WP_Query($params);
+      ?>
+
+      <?php if ($wc_query->have_posts()) : ?>
+      <?php while ($wc_query->have_posts()) : $wc_query->the_post(); ?>
+        <div class="product-card product-slider__card">
+        <a href="<?php the_permalink() ?>">
+            <?php the_post_thumbnail();?>
+          </a>
+          
+          <div class="product-info">
+            <a href="<?php the_permalink() ?>">
+              <div class="product-info__info">
+                <p class="product-info__category">
+                  <!-- Category  -->
+									<?php
+									$terms = get_the_terms( $product->ID, 'product_cat' );
+									foreach ($terms as $term) {
+											$product_cat_id = $term->term_id;
+											echo get_the_category_by_ID($product_cat_id);
+											break;
+									} 
+									?>
+                </p>
+                <p class="product-info__name">
+                  <?php the_title(); ?>
+                </p>
+                <div class="product-info__price">
+                  <?php echo wc_price($product->get_price()); ?>
+
+                  <?php if($product->is_on_sale()) : ?>
+                    <span class="price__discount">
+                      <?php echo wc_price($product->get_regular_price() ); ?>
+                    </span>
+                  <?php endif; ?>
+                </div>
+              </div>
+            </a>
+
+						<?php do_action( 'woocommerce_after_shop_loop_item' ); 
+						?>
+          </div>
+        </div>
+      <?php endwhile; ?>
+      <?php wp_reset_postdata(); ?>
+      <?php else:  ?>
+      <p>
+        <?php _e( 'No Products'); ?>
+      </p>
+      <?php endif; ?>
+    </div>
+  </div>
+
+  <?php 
+      get_template_part('template-parts/community-slider');
     ?>
   </main>
 <?php get_footer(); ?>
--- wp-content/themes/eyeconbeauty/js/carousel.js
+++ wp-content/themes/eyeconbeauty/js/carousel.js
@@ -1,11 +1,23 @@
-
 var galleryElems = document.querySelectorAll('.carousel');
 
 for ( var i=0, len = galleryElems.length; i < len; i++ ) {
   var galleryElem = galleryElems[i];
   new Flickity( galleryElem, {
     // options...
-    "wrapAround": true,
+    wrapAround: true,
+    contain: true
+  });
+};
+
+var productElems = document.querySelectorAll('.product-slider__wrapper');
+
+for ( var i=0, len = productElems.length; i < len; i++ ) {
+  var productElem = productElems[i];
+  new Flickity( productElem, {
+    // options...
+    cellAlign: 'left',
+    wrapAround: true,
+    prevNextButtons: false,
     contain: true
   });
 };
