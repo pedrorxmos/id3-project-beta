815d723322d61567b56a59c6653e7980025acc3b
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-23 00:55:56 +0100

    💄 feat: alternatives contacts template-contact

--- wp-content/themes/eyeconbeauty/template-contact.php
+++ wp-content/themes/eyeconbeauty/template-contact.php
@@ -7,7 +7,25 @@
       <h2><?php the_title(); ?></h2>
     </div>
     <div class="contact-page">
-      <?php the_content() ?>
+      <section class="contact-page__section">
+        <p>
+          Nulla quis lorem ut libero malesuada feugiat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
+        </p> 
+        <div class="section__contacts">
+          <?php while ( have_rows ( 'contact-ways' ) ) : the_row(); ?>
+            <div class="contact--line">
+            <?php echo get_sub_field ( 'contact_svg' ); ?>
+            <?php echo get_sub_field ( 'contact_value' ); ?>
+            </div>
+          <?php endwhile; ?>
+        </div>
+      </section>
+      <section class="contact-page__section">
+        <p>
+          Nulla quis lorem ut libero malesuada feugiat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.
+        </p> 
+        <?php the_content() ?>
+      </section>
     </div>
   </main>
 <?php get_footer(); ?>
