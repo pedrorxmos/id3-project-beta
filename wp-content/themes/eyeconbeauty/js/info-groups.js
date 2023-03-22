2ecfcacd8756e365783e62009a4f6171c387928b
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-22 19:29:32 +0100

    💄 feat: add animation to info groups

--- wp-content/themes/eyeconbeauty/js/info-groups.js
+++ wp-content/themes/eyeconbeauty/js/info-groups.js
@@ -3,5 +3,11 @@ const infoGroups = document.querySelectorAll('.info-group__toggle');
 infoGroups.forEach(gr => {
   gr.addEventListener('click', (event) => {
     event.target.parentElement.classList.toggle('open');
+    const infoContent = event.target.nextElementSibling;
+    if (infoContent.style.maxHeight) {
+      infoContent.style.maxHeight = null;
+    } else {
+      infoContent.style.maxHeight = infoContent.scrollHeight + "px";
+    }
   })
 })
