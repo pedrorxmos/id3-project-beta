b52f75ef8ec1d393de46f9453a085dfcd37471f3
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-23 19:59:12 +0100

    💄 feat: reverse order cart-collaterals

--- wp-content/themes/eyeconbeauty/woocommerce/cart/cross-sells.php
+++ wp-content/themes/eyeconbeauty/woocommerce/cart/cross-sells.php
@@ -19,31 +19,33 @@ defined( 'ABSPATH' ) || exit;
 
 if ( $cross_sells ) : ?>
 
-	<div class="cross-sells">
+	<div class="product-slider cross-sells">
 		<?php
 		$heading = apply_filters( 'woocommerce_product_cross_sells_products_heading', __( 'You may be interested in&hellip;', 'woocommerce' ) );
 
 		if ( $heading ) :
 			?>
-			<h2><?php echo esc_html( $heading ); ?></h2>
+			<div class="product-slider-title">
+				<h2><?php echo esc_html( $heading ); ?></h2>
+			</div>
 		<?php endif; ?>
 
-		<?php woocommerce_product_loop_start(); ?>
+		<div class="product-slider__wrapper left-carousel">
+		
 
-			<?php foreach ( $cross_sells as $cross_sell ) : ?>
+		<?php foreach ( $cross_sells as $cross_sell ) : ?>
 
-				<?php
-					$post_object = get_post( $cross_sell->get_id() );
+			<?php
+				$post_object = get_post( $cross_sell->get_id() );
 
-					setup_postdata( $GLOBALS['post'] =& $post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
+				setup_postdata( $GLOBALS['post'] =& $post_object ); // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited, Squiz.PHP.DisallowMultipleAssignments.Found
 
-					wc_get_template_part( 'content', 'product' );
-				?>
+				wc_get_template_part( 'content', 'product' );
+			?>
 
-			<?php endforeach; ?>
+		<?php endforeach; ?>
 
-		<?php woocommerce_product_loop_end(); ?>
-
+		</div>
 	</div>
 	<?php
 endif;
