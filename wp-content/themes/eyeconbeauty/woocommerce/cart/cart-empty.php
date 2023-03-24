ebf4925b17401bd1350c875f607b148f61e2d22d
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-24 15:41:07 +0100

    💄 feat: empty cart

--- wp-content/themes/eyeconbeauty/woocommerce/cart/cart-empty.php
+++ wp-content/themes/eyeconbeauty/woocommerce/cart/cart-empty.php
@@ -20,12 +20,11 @@ defined( 'ABSPATH' ) || exit;
 /*
  * @hooked wc_empty_cart_message - 10
  */
-do_action( 'woocommerce_cart_is_empty' );
 
 if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
-	
-	<p class="return-to-shop">
-		<a class="button wc-backward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
+	<div class="empty_cart_wrapper">
+		<?php do_action( 'woocommerce_cart_is_empty' );?>
+		<a class="btn btn-main return-to-shop button wc-backward<?php echo esc_attr( wc_wp_theme_get_element_class_name( 'button' ) ? ' ' . wc_wp_theme_get_element_class_name( 'button' ) : '' ); ?>" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
 			<?php
 				/**
 				 * Filter "Return To Shop" text.
@@ -36,5 +35,5 @@ if ( wc_get_page_id( 'shop' ) > 0 ) : ?>
 				echo esc_html( apply_filters( 'woocommerce_return_to_shop_text', __( 'Return to shop', 'woocommerce' ) ) );
 			?>
 		</a>
-	</p>
+	</div>
 <?php endif; ?>
