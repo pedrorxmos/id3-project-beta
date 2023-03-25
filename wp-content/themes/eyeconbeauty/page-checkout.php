9db0da39ffd544c0930b5ab23452db84221749c0
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-25 13:13:40 +0100

    💄 feat: header checkout

---
+++ wp-content/themes/eyeconbeauty/page-checkout.php
@@ -0,0 +1,16 @@
+<?php get_header( 'checkout' ); the_post();?>
+  <main>
+    <h1 class="hide"><?php the_title(); ?></h1>
+    <!-- <div class="container">
+      <h2><?php the_title(); ?></h2>
+      <?php 
+        $subtitle = get_field('page_subtitle');
+        if ( $subtitle ) : 
+      ?>
+        <h3><?php the_field( 'page_subtitle' );; ?></h3>
+      <?php endif; ?>
+      
+    </div> -->
+    <?php the_content() ?>
+  </main>
+<?php get_footer(); ?>
