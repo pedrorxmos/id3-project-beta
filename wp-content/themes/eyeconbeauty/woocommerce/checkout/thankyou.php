3b700799772a110128c049583b68b5ab89b0578d
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-26 18:08:59 +0200

    💄 feat: order received thank you text

--- wp-content/themes/eyeconbeauty/woocommerce/checkout/thankyou.php
+++ wp-content/themes/eyeconbeauty/woocommerce/checkout/thankyou.php
@@ -27,52 +27,61 @@ defined( 'ABSPATH' ) || exit;
 		?>
 
 		<?php if ( $order->has_status( 'failed' ) ) : ?>
+			<div class="thankyou-text">
+				<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
 
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed"><?php esc_html_e( 'Unfortunately your order cannot be processed as the originating bank/merchant has declined your transaction. Please attempt your purchase again.', 'woocommerce' ); ?></p>
-
-			<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
-				<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
-				<?php if ( is_user_logged_in() ) : ?>
-					<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
-				<?php endif; ?>
-			</p>
+				<p class="woocommerce-notice woocommerce-notice--error woocommerce-thankyou-order-failed-actions">
+					<a href="<?php echo esc_url( $order->get_checkout_payment_url() ); ?>" class="button pay"><?php esc_html_e( 'Pay', 'woocommerce' ); ?></a>
+					<?php if ( is_user_logged_in() ) : ?>
+						<a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>" class="button pay"><?php esc_html_e( 'My account', 'woocommerce' ); ?></a>
+					<?php endif; ?>
+				</p>
+			</div>
+			
 
 		<?php else : ?>
 
-			<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you. Your order has been received.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<div class="thankyou-text">
+				<p class="thankyou__title woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', esc_html__( 'Thank you for your order.', 'woocommerce' ), $order ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+				<p class="thankyou_confirmation">
+					<?php _e( 'An email confirmation has been sent to your email.', 'woocommerce' ) ?>
+				</p>
+			</div>	
 
-			<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
-
-				<li class="woocommerce-order-overview__order order">
-					<?php esc_html_e( 'Order number:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
-
-				<li class="woocommerce-order-overview__date date">
-					<?php esc_html_e( 'Date:', 'woocommerce' ); ?>
-					<strong><?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
-
-				<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
-					<li class="woocommerce-order-overview__email email">
-						<?php esc_html_e( 'Email:', 'woocommerce' ); ?>
-						<strong><?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
+			<section class="thankyou-section">
+				<ul class="woocommerce-order-overview woocommerce-thankyou-order-details order_details">
+					<li class="woocommerce-order-overview__order order">
+					<strong><?php esc_html_e( 'Order number:', 'woocommerce' ); ?></strong>
+						<?php echo $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 					</li>
-				<?php endif; ?>
 
-				<li class="woocommerce-order-overview__total total">
-					<?php esc_html_e( 'Total:', 'woocommerce' ); ?>
-					<strong><?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></strong>
-				</li>
-
-				<?php if ( $order->get_payment_method_title() ) : ?>
-					<li class="woocommerce-order-overview__payment-method method">
-						<?php esc_html_e( 'Payment method:', 'woocommerce' ); ?>
-						<strong><?php echo wp_kses_post( $order->get_payment_method_title() ); ?></strong>
+					<li class="woocommerce-order-overview__date date">
+					<strong><?php esc_html_e( 'Date:', 'woocommerce' ); ?></strong>
+						<?php echo wc_format_datetime( $order->get_date_created() ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 					</li>
-				<?php endif; ?>
 
-			</ul>
+					<?php if ( is_user_logged_in() && $order->get_user_id() === get_current_user_id() && $order->get_billing_email() ) : ?>
+						<li class="woocommerce-order-overview__email email">
+						<strong><?php esc_html_e( 'Email:', 'woocommerce' ); ?></strong>
+							<?php echo $order->get_billing_email(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+						</li>
+					<?php endif; ?>
+
+					<li class="woocommerce-order-overview__total total">
+					<strong><?php esc_html_e( 'Total:', 'woocommerce' ); ?></strong>
+						<?php echo $order->get_formatted_order_total(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
+					</li>
+
+					<?php if ( $order->get_payment_method_title() ) : ?>
+						<li class="woocommerce-order-overview__payment-method method">
+						<strong><?php esc_html_e( 'Payment method:', 'woocommerce' ); ?></strong>
+							<?php echo wp_kses_post( $order->get_payment_method_title() ); ?>
+						</li>
+					<?php endif; ?>
+
+				</ul>
+			</section>
+			
 
 		<?php endif; ?>
 
