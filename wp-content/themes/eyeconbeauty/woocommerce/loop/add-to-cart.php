650abbb4e05cd366f32552b13a5395d8f050b574
Pedro Ramos <pedroaramosguardiola@gmail.com>
2023-03-27 22:16:41 +0200

    💄 feat: product list and card media queries

--- wp-content/themes/eyeconbeauty/woocommerce/loop/add-to-cart.php
+++ wp-content/themes/eyeconbeauty/woocommerce/loop/add-to-cart.php
@@ -24,12 +24,13 @@ global $product;
 echo apply_filters(
 	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
 	sprintf(
-		'<a href="%s" data-quantity="%s" class="btn btn-secondary %s" %s>%s</a>',
+		'<a href="%s" data-quantity="%s" class="btn btn-secondary %s" %s>%s <span class="price">%s</span></a>',
 		esc_url( $product->add_to_cart_url() ),
 		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
 		esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
 		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-		esc_html( $product->add_to_cart_text() )
+		esc_html( $product->add_to_cart_text() ),
+		wc_price($product->get_price())
 	),
 	$product,
 	$args
