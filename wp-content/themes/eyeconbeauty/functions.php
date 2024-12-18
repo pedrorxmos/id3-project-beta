4a1f50ae0c7f4a23c3472d96d3774d6e24051310
Pedro Ramos <pedrorxmosdev@gmail.com>
2024-12-18 20:03:30 +0100

    🔨 update scripts version

--- wp-content/themes/eyeconbeauty/functions.php
+++ wp-content/themes/eyeconbeauty/functions.php
@@ -26,11 +26,11 @@
 			// wp_enqueue_script ( 'js-topbar', get_stylesheet_directory_uri() . './js/topbar.js' );
 	
 			// wp_enqueue_style ( 'css-main', get_stylesheet_uri() );
-			wp_enqueue_script ( 'js-topbar', get_stylesheet_directory_uri() . '/js/topbar.js', array('jquery'), '1.0.0', true, true );
-			wp_enqueue_script ( 'js-carousel', get_stylesheet_directory_uri() . '/js/carousel.js', array('jquery'), '1.0.0', true, true );
-			wp_enqueue_script ( 'js-variation', get_stylesheet_directory_uri() . '/js/get-actual-variation.js', array('jquery'), '1.0.0', true, true );
-			wp_enqueue_script ( 'js-dropdown', get_stylesheet_directory_uri() . '/js/dropdown.js', array('jquery'), '1.0.0', true, true );
-			wp_enqueue_script ( 'js-productwrapper', get_stylesheet_directory_uri() . '/js/product-wrapper.js', array('jquery'), '1.0.0', true, true );
+			wp_enqueue_script ( 'js-topbar', get_stylesheet_directory_uri() . '/js/topbar.js', array('jquery'), time(), true, true );
+			wp_enqueue_script ( 'js-carousel', get_stylesheet_directory_uri() . '/js/carousel.js', array('jquery'), time(), true, true );
+			wp_enqueue_script ( 'js-variation', get_stylesheet_directory_uri() . '/js/get-actual-variation.js', array('jquery'), time(), true, true );
+			wp_enqueue_script ( 'js-dropdown', get_stylesheet_directory_uri() . '/js/dropdown.js', array('jquery'), time(), true, true );
+			wp_enqueue_script ( 'js-productwrapper', get_stylesheet_directory_uri() . '/js/product-wrapper.js', array('jquery'), time(), true, true );
 
 			wp_enqueue_style ( 'css-main', get_stylesheet_uri(), '', filemtime ( get_template_directory() . '/style.css' ) );
 
